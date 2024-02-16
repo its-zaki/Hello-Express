fe08eed24a7c07a2a0c49a999a48c108b307eb29
its zaki <zakiyounus789@gmail.com>
2024-02-17 01:46:20 +0500

    Hello World ! on Express js

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('<h1>Hello World !</h1>')
 })
-app.get('/login', (req, res) => {
-  res.send('Hello Login!')
-})
-app.get('/home', (req, res) => {
-  res.send('Hello Home!')
-})
+
+
 
 app.listen(process.env.PORTS, () => {
   console.log(`Example app listening on port ${process.env.PORTS}`)
